50eb10fbedfdd17b5a7a1780503c3dc4797bf027
vipinn123 <vipinn@gmail.com>
2020-06-24 19:31:30 +0530

    ssl check-in 1

--- src/main/java/com/spring/authenticator/AuthenticatorSpringApplication.java
+++ src/main/java/com/spring/authenticator/AuthenticatorSpringApplication.java
@@ -1,9 +1,16 @@
 package com.spring.authenticator;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -13,9 +20,19 @@ public class AuthenticatorSpringApplication {
 	private static Logger logger = LoggerFactory.getLogger(AuthenticatorSpringApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthenticatorSpringApplication.class, args);
+		//SpringApplication.run(AuthenticatorSpringApplication.class, args);
+		
+		// spring boot 2.x
+		SpringApplication application = new SpringApplication(AuthenticatorSpringApplication.class);
+        application.setAdditionalProfiles("ssl");
+        application.run(args);
+		
+		
 		
 		logger.info("Authenticator Application Started");
 	}
+	
+	
+
 
 }
