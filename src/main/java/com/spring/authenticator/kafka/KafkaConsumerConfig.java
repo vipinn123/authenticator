d5c477c19fde78a46c950823ff833ffba53f88ad
vipinn123 <vipinn@gmail.com>
2020-06-26 20:17:42 +0530

    kafka Producer

--- src/main/java/com/spring/authenticator/kafka/KafkaConsumerConfig.java
+++ src/main/java/com/spring/authenticator/kafka/KafkaConsumerConfig.java
@@ -23,7 +23,7 @@ public class KafkaConsumerConfig {
 	@Bean
 	   public ConsumerFactory<String, String> consumerFactory() {
 	      Map<String, Object> props = new HashMap<>();
-	      props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+	      props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "hello-spring-boot-kafka-kafka-bootstrap:9092");
 	      props.put(ConsumerConfig.GROUP_ID_CONFIG, "full-read");
 	      props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 	      props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
